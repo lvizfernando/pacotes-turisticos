bd10dd05a3a7ad3ec8187bf0fbd9dc610be372e6
Luiz Fernando <404.luiz@gmail.com>
2022-09-01 18:14:17 -0300

    commit inicial

---
+++ src/main/java/br/ufscar/dc/dsw/PacotesTuristicosApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacotesTuristicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacotesTuristicosApplication.class, args);
+	}
+
+}
